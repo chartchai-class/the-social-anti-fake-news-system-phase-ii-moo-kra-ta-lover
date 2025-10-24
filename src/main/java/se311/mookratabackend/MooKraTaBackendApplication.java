c95de62524c8f593c2b7677850ca0fb1ac7726cf
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-10-24 13:51:00 +0700

    initial commit - spring setup

---
+++ src/main/java/se311/mookratabackend/MooKraTaBackendApplication.java
@@ -0,0 +1,13 @@
+package se311.mookratabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooKraTaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MooKraTaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se311/mookratabackend/MooKraTaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se311.mookratabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooKraTaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
