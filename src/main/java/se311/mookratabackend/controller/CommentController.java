e6af89721a4bdd062fbc7bf12cba740cbd5c3ebc
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-10-24 18:59:34 +0700

    fix(news): fix comment controller

--- src/main/java/se311/mookratabackend/controller/CommentController.java
+++ src/main/java/se311/mookratabackend/controller/CommentController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import se311.mookratabackend.entity.Comment;
@@ -14,14 +15,19 @@ import se311.mookratabackend.util.LabMapper;
 @RequiredArgsConstructor
 public class CommentController {
     final CommentService commentService;
-    @PostMapping("/news")
+    @PostMapping("/comments")
     public ResponseEntity<?> addEvent(@RequestBody Comment comment){
         Comment output = commentService.save(comment);
         return ResponseEntity.ok(LabMapper.INSTANCE.getCommentDto(output));
     }
 
-    @DeleteMapping
-    public ResponseEntity<?> deleteEvent(@RequestBody Comment comment){
-        Comment output = commentService.
+    @DeleteMapping("/comments/{id}")
+    public ResponseEntity<?> deleteEvent(@PathVariable("id") Long id){
+        Comment comment = commentService.getComments(id);
+        if (comment == null) {
+            return ResponseEntity.notFound().build();
+        }
+        commentService.delete(comment);
+        return ResponseEntity.noContent().build();
     }
 }
